329552e54552c4a8d9ade6c9d5ac744b67276fca
Raimundo Andrade de Lima Neto <andradeneto1999@gmail.com>
2022-01-24 18:24:48 -0300

    .
    ..

--- Untitled-1.py
+++ Untitled-1.py
@@ -44,8 +44,8 @@ logo = Image.open('images/youtube.png')
 logo = logo.resize((50, 50), Image.ANTIALIAS)
 logo = ImageTk.PhotoImage(logo)
 
-l_logo = Label(frame_cima, image=logo, compund=LEFT,
-               bg=fundo, font=('Ivy 10 bold'), anchar='nw')
+l_logo = Label(frame_cima, image=logo, compound=LEFT,
+               bg=fundo, font=('Ivy 10 bold'), anchor='nw')
 l_logo.place(x=5, y=5)
 
 l_name = Label(frame_cima, text="Youtube Dowload app", width=32,
@@ -54,6 +54,7 @@ l_name.place(x=65, y=15)
 
 
 def pesquisar():
+    global img
     # pegando o link
     url = e_url.get()
     yt = YouTube(url)
@@ -74,7 +75,7 @@ def pesquisar():
     foto = yt.thumbnail_url
 
     img = Image.open(requests.get(foto, stream=True).raw)
-    img = img.resize((20, 20), Image.ANTIALIAS)
+    img = img.resize((230, 150), Image.ANTIALIAS)
     img = ImageTk.PhotoImage(img)
 
     l_imagem['image'] = img
@@ -96,8 +97,8 @@ b_pesquisar.place(x=404, y=80)
 
 # operações
 
-l_imagem = Label(frame_baixo, image=logo, compund=LEFT,
-                 bg=fundo, font=('Ivy 10 bold'), anchar='nw')
+l_imagem = Label(frame_baixo, image=logo, compound=LEFT,
+                 bg=fundo, font=('Ivy 10 bold'), anchor='nw')
 l_imagem.place(x=5, y=5)
 
 l_titlo = Label(frame_baixo, text="titlo do video", height=2, wraplength=225, compound=LEFT,
@@ -113,9 +114,9 @@ l_time = Label(frame_baixo, text="00:02:50",
 l_time.place(x=250, y=85)
 
 
-down = Image.open('images/download_2.png')
-down = down.resize((50, 50), Image.ANTIALIAS)
-down = ImageTk.PhotoImage(down)
+#down = Image.open('images/download_2.png')
+#down = down.resize((50, 50), Image.ANTIALIAS)
+#down = ImageTk.PhotoImage(down)
 
 b_down = Button(frame_baixo, text="Dowload", compound=LEFT,
                 bg=fundo, font=('Ivy 10 bold'), overrelief=RIDGE)
