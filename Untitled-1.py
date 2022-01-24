48d7f3126f0b611143416bfca7ae0702f730a20b
Raimundo Andrade de Lima Neto <andradeneto1999@gmail.com>
2022-01-24 17:35:03 -0300

    .
    .

--- Untitled-1.py
+++ Untitled-1.py
@@ -4,7 +4,7 @@ from urllib import request
 
 # importando pillow
 
-#from PIL import image, ImageTk
+from PIL import Image, ImageTk
 
 from pytube import YouTube
 
@@ -40,12 +40,13 @@ frame_baixo.grid(row=2, column=0, sticky=NW)
 
 # configurando o frame cima
 
-#logo = Image.open('images/youtube.png')
-#logo = logo.resize((50,50), Image.ANTIALIAS)
-#logo = ImageTk.PhotoImage(logo)
+logo = Image.open('images/youtube.png')
+logo = logo.resize((50, 50), Image.ANTIALIAS)
+logo = ImageTk.PhotoImage(logo)
 
-#l_logo = Label(frame_cima, image=logo, compund=LEFT, bg=fundo, font=('Ivy 10 bold'), anchar='nw')
-#l_logo.place(x=5, y=5)
+l_logo = Label(frame_cima, image=logo, compund=LEFT,
+               bg=fundo, font=('Ivy 10 bold'), anchar='nw')
+l_logo.place(x=5, y=5)
 
 l_name = Label(frame_cima, text="Youtube Dowload app", width=32,
                bg=fundo, fg=co1, font=('Ivy 10 bold'), anchor='nw')
@@ -73,6 +74,10 @@ def pesquisar():
     foto = yt.thumbnail_url
 
     img = Image.open(requests.get(foto, stream=True).raw)
+    img = img.resize((20, 20), Image.ANTIALIAS)
+    img = ImageTk.PhotoImage(img)
+
+    l_imagem['image'] = img
 
     l_titlo['text'] = "Titulo :" + titlo
     l_views['text'] = "Views :" + str('{:,}'.format(view))
@@ -91,9 +96,9 @@ b_pesquisar.place(x=404, y=80)
 
 # operações
 
-# l_imagem = Label(frame_baixo, image=logo, compund=LEFT,
-#               bg=fundo, font=('Ivy 10 bold'), anchar='nw')
-#l_imagem.place(x=5, y=5)
+l_imagem = Label(frame_baixo, image=logo, compund=LEFT,
+                 bg=fundo, font=('Ivy 10 bold'), anchar='nw')
+l_imagem.place(x=5, y=5)
 
 l_titlo = Label(frame_baixo, text="titlo do video", height=2, wraplength=225, compound=LEFT,
                 bg=fundo, fg=co1, font=('Ivy 10 bold'), anchor='nw')
@@ -108,9 +113,9 @@ l_time = Label(frame_baixo, text="00:02:50",
 l_time.place(x=250, y=85)
 
 
-#down = Image.open('images/download_2.png')
-#down = down.resize((50, 50), Image.ANTIALIAS)
-#down = ImageTk.PhotoImage(down)
+down = Image.open('images/download_2.png')
+down = down.resize((50, 50), Image.ANTIALIAS)
+down = ImageTk.PhotoImage(down)
 
 b_down = Button(frame_baixo, text="Dowload", compound=LEFT,
                 bg=fundo, font=('Ivy 10 bold'), overrelief=RIDGE)
